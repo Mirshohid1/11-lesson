572b684233c607780831defa4222dad5276ffa67
l0n3r <mirshohid1214@gmail.com>
2024-10-08 17:22:38 +0500

    in progress

--- university/urls.py
+++ university/urls.py
@@ -6,14 +6,28 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('faculties/', views.faculty_list, name='faculties'),
     path('faculty_form/', views.faculty_add, name='faculty_form'),
+
     path('groups/', views.group_list, name='groups'),
     path('group_form/', views.group_add, name='group_form'),
+
     path('department_form/', views.department_add, name='department_form'),
     path('departments/', views.department_list, name='departments'),
+
     path('student_form/', views.student_add, name='student_form'),
     path('students/', views.student_list, name='students'),
+
     path('subject_form/', views.subject_add, name='subject_form'),
     path('subjects/', views.subject_list, name='subjects'),
+
     path('teacher/', views.teacher_add, name='teacher_form'),
     path('teachers', views.teacher_list, name='teachers'),
+
+    path('delete_faculty/<int:pk>/', views.del_faculty, name='del_faculty'),
+    path('delete_group/<int:pk>/', views.del_group, name='del_group'),
+    path('delete_department/<int:pk>/', views.del_department, name='del_department'),
+    path('delete_student/<int:pk>/', views.del_student, name='del_student'),
+    path('delete_subject/<int:pk>/', views.del_subject, name='del_subject'),
+    path('delete_teacher/<int:pk>/', views.del_teacher, name='del_teacher'),
+
+
 ]
--- university/views.py
+++ university/views.py
@@ -2,7 +2,7 @@ from logging import fatal
 from math import factorial
 
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import FacultyForm, GroupForm, DepartmentForm, StudentForm, SubjectForm, TeacherForm
 from .models import Faculty, Group, Department, Student, Subject, Teacher
 
@@ -23,7 +23,7 @@ def dashboard(request):
 
 def faculty_list(request):
     faculties = Faculty.objects.all()
-    return render(request, 'faculty_list.html', {'faculties': faculties})
+    return render(request, 'lists/faculty_list.html', {'faculties': faculties})
 
 def faculty_add(request):
     if request.method == 'POST':
@@ -35,11 +35,11 @@ def faculty_add(request):
             return HttpResponse("Form is invalid.")
     form = FacultyForm()
     ctx = {'faculty': form}
-    return render(request, 'faculty_form.html', ctx)
+    return render(request, 'forms/faculty_form.html', ctx)
 
 def group_list(request):
     groups = Group.objects.all()
-    return render(request, 'group_list.html', {'groups': groups})
+    return render(request, 'lists/group_list.html', {'groups': groups})
 
 def group_add(request):
     if request.method == 'POST':
@@ -51,11 +51,11 @@ def group_add(request):
             return HttpResponse("Form is invalid.")
     group = GroupForm()
     ctx = {'group': group}
-    return render(request, 'group_form.html', ctx)
+    return render(request, 'forms/group_form.html', ctx)
 
 def department_list(request):
     departments = Department.objects.all()
-    return render(request, 'department_list.html', {'departments': departments})
+    return render(request, 'lists/department_list.html', {'departments': departments})
 
 def department_add(request):
     if request.method == 'POST':
@@ -67,11 +67,11 @@ def department_add(request):
             return HttpResponse("Form is invalid.")
     department = DepartmentForm()
     ctx = {'department': department}
-    return render(request, 'department_form.html', ctx)
+    return render(request, 'forms/department_form.html', ctx)
 
 def student_list(request):
     students = Student.objects.all()
-    return render(request, 'student_list.html', {'students': students})
+    return render(request, 'lists/student_list.html', {'students': students})
 
 def student_add(request):
     if request.method == 'POST':
@@ -83,11 +83,11 @@ def student_add(request):
             return HttpResponse("Form is invalid.")
     student = StudentForm()
     ctx = {'student': student}
-    return render(request, 'student_form.html', ctx)
+    return render(request, 'forms/student_form.html', ctx)
 
 def subject_list(request):
     subjects = Subject.objects.all()
-    return render(request, 'subject_list.html', {'subjects': subjects})
+    return render(request, 'lists/subject_list.html', {'subjects': subjects})
 
 def subject_add(request):
     if request.method == 'POST':
@@ -99,11 +99,11 @@ def subject_add(request):
             return HttpResponse("Form is invalid.")
     subject = SubjectForm()
     ctx = {'subject': subject}
-    return render(request, 'subject_form.html', ctx)
+    return render(request, 'forms/subject_form.html', ctx)
 
 def teacher_list(request):
     teachers = Teacher.objects.all()
-    return render(request, 'teacher_list.html', {'teachers': teachers})
+    return render(request, 'lists/teacher_list.html', {'teachers': teachers})
 
 def teacher_add(request):
     if request.method == 'POST':
@@ -115,4 +115,53 @@ def teacher_add(request):
             return HttpResponse("Form is invalid.")
     teacher = TeacherForm()
     ctx = {'teacher': teacher}
-    return render(request, 'teacher_form.html', ctx)
+    return render(request, 'forms/teacher_form.html', ctx)
+
+
+def del_faculty(request, pk):
+    faculty = get_object_or_404(Faculty, pk=pk)
+    if request.method == 'POST':
+        faculty.delete()
+        return redirect('faculties')
+
+    return render(request, 'confirms/confirm_faculty.html', {'faculty': faculty})
+
+def del_group(request, pk):
+    group = get_object_or_404(Group, pk=pk)
+    if request.method == 'POST':
+        group.delete()
+        return redirect('groups')
+
+    return render(request, 'confirms/confirm_group.html', {'group': group})
+
+def del_department(request, pk):
+    department = get_object_or_404(Department, pk=pk)
+    if request.method == 'POST':
+        department.delete()
+        return redirect('departments')
+
+    return render(request, 'confirms/confirm_department.html', {'department': department})
+
+def del_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('students')
+
+    return render(request, 'confirms/confirm_student.html', {'student': student})
+
+def del_subject(request, pk):
+    subject = get_object_or_404(Subject, pk=pk)
+    if request.method == 'POST':
+        subject.delete()
+        return redirect('subjects')
+
+    return render(request, 'confirms/confirm_subject.html', {'subject': subject})
+
+def del_teacher(request, pk):
+    teacher= get_object_or_404(Teacher, pk=pk)
+    if request.method == 'POST':
+        teacher.delete()
+        return redirect('teachers')
+
+    return render(request, 'confirms/confirm_teacher.html', {'teacher': teacher})
